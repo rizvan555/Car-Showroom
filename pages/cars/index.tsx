64d4dbaf7b3ff4be4e2b6537e4d8e22670c6bb74
Rizvan <rizvan@MacBook-Air.local>
2023-07-27 06:01:49 +0400

    created backend file

--- pages/cars/index.tsx
+++ pages/cars/index.tsx
@@ -1,7 +1,14 @@
 import React from 'react';
 
 function Cars() {
-  return <div>Cars</div>;
+  return (
+    <div className="flex flex-col  h-[100vh] mx-auto px-2">
+      <div className="flex items-center gap-8  mb-20">
+        <h1 className="text-6xl font-bold">OUR CARS</h1>
+        <hr className="hr-line" />
+      </div>
+    </div>
+  );
 }
 
 export default Cars;
