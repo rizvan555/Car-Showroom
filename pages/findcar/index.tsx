90fdd352aa475e84ff645e00bbdbffce92d40087
Rizvan <rizvan@MacBook-Air.local>
2023-08-02 14:35:52 +0400

    created result search page

--- pages/findcar/index.tsx
+++ pages/findcar/index.tsx
@@ -1,17 +1,39 @@
 import axios from 'axios';
 import Image from 'next/image';
 import React, { useEffect, useState } from 'react';
+import {
+  BsFillFuelPumpDieselFill,
+  BsFillPersonFill,
+  BsSpeedometer2,
+} from 'react-icons/bs';
+import { MdAppRegistration } from 'react-icons/md';
+import { PiEngineDuotone } from 'react-icons/pi';
+import { TbRoad } from 'react-icons/tb';
 
 interface Cars {
+  _id: string;
+  img: string;
+  img1: string;
+  img2: string;
+  img3: string;
+  img4: string;
   name: string;
   model: string;
-  img: string;
+  fuel: string;
+  transmission: string;
+  displacement: number;
+  owner: number;
+  price: number;
+  km: number;
+  year: number;
+  ps: number;
 }
 
 function FindCar() {
   const [cars, setCars] = useState<Cars[]>([]);
   const [selectedName, setSelectedName] = useState<string>('');
   const [selectedModel, setSelectedModel] = useState<string>('');
+  const [filteredCars, setFilteredCars] = useState<Cars[]>([]);
 
   useEffect(() => {
     const getCars = async () => {
@@ -31,6 +53,7 @@ function FindCar() {
       (car) => car.name === selectedName && car.model === selectedModel
     );
     console.log(filteredCars);
+    setFilteredCars(filteredCars);
   };
 
   return (
@@ -68,7 +91,54 @@ function FindCar() {
       >
         SEARCH
       </button>
-   
+      {filteredCars.map((car, index) => (
+        <div className="flex gap-10 px-6 py-10 w-[72vw] border">
+          <div>
+            <Image src={car.img} alt="image" width={300} height={200} />
+          </div>
+          <div className="mr-38 w-[30vw]">
+            <div className="flex gap-3 ">
+              <p className="text-2xl font-bold"> {car.name}</p>
+              <p className="text-2xl"> {car.model}</p>
+            </div>
+            <div className="flex flex-col">
+              <p className="text-base">{car.transmission}</p>
+              <p className="flex items-center gap-3 text-l mt-6">
+                <MdAppRegistration size={22} />
+                <span className="font-bold">Erstzulassung:</span> {car.year}
+              </p>
+              <p className="flex items-center gap-4 text-l">
+                <BsFillFuelPumpDieselFill size={20} />
+                <span className="font-bold">Kraftstoff:</span> {car.fuel}
+              </p>
+              <p className="flex items-center gap-3 text-l">
+                <TbRoad size={20} />
+                <span className="font-bold">Kilometerstand:</span> {car.km} km
+              </p>
+              <p className="flex items-center gap-3 text-l">
+                <PiEngineDuotone size={22} />
+                <span className="font-bold">Hubraum:</span> {car.displacement}{' '}
+                cm
+              </p>
+              <p className="flex items-center gap-3 text-l">
+                <BsSpeedometer2 size={20} />
+                <span className="font-bold">Leistung:</span> {car.ps} hp
+              </p>
+              <p className="flex items-center gap-3 text-l">
+                <BsFillPersonFill size={20} />
+                <span className="font-bold">Farhrzeughalter:</span> {car.owner}
+              </p>
+            </div>
+          </div>
+          <div>
+            <p className="text-3xl mb-6">
+              {car.price}
+              <span className="text-primary ml-2">€</span>
+            </p>
+          </div>
+          <hr />
+        </div>
+      ))}
     </div>
   );
 }
