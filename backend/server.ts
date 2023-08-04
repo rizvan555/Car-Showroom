c1217c6d47b680dd8c010e7715bd03b14e7850b7
Rizvan <rizvan@MacBook-Air.local>
2023-08-04 06:54:22 +0400

    added extra filter for year

--- backend/server.ts
+++ backend/server.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import { Schema, model } from 'mongoose';
 import cors from 'cors';
+import '@types/express';
 
 mongoose.connect('mongodb://localhost:27017/cars');
 
