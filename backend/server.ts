236dc405e38c635f846a3b08aac1e6251386b33e
Rizvan <rizvan@MacBook-Air.local>
2023-08-12 06:48:43 +0400

    created new json folders for different infos

--- backend/server.ts
+++ backend/server.ts
@@ -2,6 +2,10 @@ import express from 'express';
 import mongoose from 'mongoose';
 import { Schema, model } from 'mongoose';
 import cors from 'cors';
+import bodyParser from 'body-parser';
+import cookieParser from 'cookie-parser';
+import Multer from 'multer';
+import multer from 'multer';
 
 mongoose.connect('mongodb://localhost:27017/cars');
 mongoose.createConnection('mongodb://localhost:27017/caremployment');
@@ -11,6 +15,26 @@ const port = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(cors());
+app.use(cookieParser());
+app.use(bodyParser.json({ limit: '50mb' }));
+app.use(
+  bodyParser.urlencoded({
+    limit: '50mb',
+    extended: true,
+    parameterLimit: 50000,
+  })
+);
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
+
+const storage: Multer.StorageEngine = multer.memoryStorage();
+const upload = Multer({
+  storage,
+});
 
 const carsSchema = new Schema({
   img: String,
