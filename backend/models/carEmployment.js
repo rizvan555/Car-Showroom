983bc2e7450221d4876866a8adef0ce7dddcc0fb
Rizvan <rizvan@MacBook-Air-2.fritz.box>
2023-09-19 22:44:09 +0200

    commit

--- backend/models/carEmployment.js
+++
@@ -1,8 +0,0 @@
-import mongoose, { Schema } from 'mongoose';
-
-const carEmploymentSchema = new Schema({
-  name: String,
-  models: [String],
-});
-export default mongoose.models.CarEmployment ||
-  mongoose.model('CarEmployment', carEmploymentSchema);
